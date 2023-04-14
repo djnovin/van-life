c534fd7e86537eb4b3e085824129a3aebe8c7b1f
djnovin <hello@novinnoori.com>
2023-04-14 14:45:58 +1000

    added some stuff

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
 import { useState } from 'react';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  useLocation,
+} from 'react-router-dom';
 import Vans from './pages/Vans';
 import About from './pages/About';
 import Home from './pages/Home';
+import { motion, AnimatePresence } from 'framer-motion';
 
 import './App.css';
 import Van from './pages/Van';
@@ -20,24 +27,26 @@ function App() {
         </nav>
         <div></div>
       </header>
-      <Routes>
-        <Route
-          path='/'
-          element={<Home />}
-        />
-        <Route
-          path='/vans/:id'
-          element={<Van />}
-        />
-        <Route
-          path='/vans'
-          element={<Vans />}
-        />
-        <Route
-          path='/about'
-          element={<About />}
-        />
-      </Routes>
+      <AnimatePresence mode='wait'>
+        <Routes>
+          <Route
+            path='/'
+            element={<Home />}
+          />
+          <Route
+            path='/vans/:id'
+            element={<Van />}
+          />
+          <Route
+            path='/vans'
+            element={<Vans />}
+          />
+          <Route
+            path='/about'
+            element={<About />}
+          />
+        </Routes>
+      </AnimatePresence>
     </BrowserRouter>
   );
 }
